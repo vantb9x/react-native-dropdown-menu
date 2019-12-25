48a7344fa922b3292c8a4bb3f29e3a08f8762903
vannguyen <vantb9x@gmail.com>
2019-12-25 10:52:46 +0700

    css image

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ class DropdownMenu extends Component {
       <ScrollView style={[{position: 'absolute', top: 0, left: 0, right: 0, backgroundColor: 'white'}, heightStyle, this.props.panelStyle]} >
       {
         currentTitles.map((title, index) =>
-        <TouchableOpacity key={index} activeOpacity={1} style={{flex: 1, height: 44}} onPress={this.itemOnPress.bind(this, index)} >
+        <TouchableOpacity key={index} activeOpacity={1} style={{flex: 1, height: 50}} onPress={this.itemOnPress.bind(this, index)} >
         {this.renderChcek(index, title)}
       <View style={{backgroundColor: '#F6F6F6', height: 1, marginLeft: 15}} />
       </TouchableOpacity>
